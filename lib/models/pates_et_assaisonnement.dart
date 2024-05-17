86ed2c1bef48ba1b890b764deda59386d1e55b2f
lrafael <rafael.lafon@ynov.com>
2024-05-17 09:21:44 +0200

    ea

--- lib/models/pates_et_assaisonnement.dart
+++ lib/models/pates_et_assaisonnement.dart
@@ -2,21 +2,24 @@ class PatesEtAssaisonnement{
   String Name;
   int Quantity;
   int Weight;
+  int Liter;
 
 
-  PatesEtAssaisonnement(this.Name, this.Quantity, this.Weight);
+  PatesEtAssaisonnement(this.Name, this.Quantity, this.Weight, this.Liter);
 
   Map<String, dynamic> toMap() {
     return {
       'name' : Name,
       'quantity' : Quantity,
-      'weight' : Weight
+      'weight' : Weight,
+      'liter' : Liter
     };
   }
 
   factory PatesEtAssaisonnement.fromMap (Map<String, dynamic> map) => PatesEtAssaisonnement(
     map['name'],
     map['quantity'],
-    map['weight']
+    map['weight'],
+    map['liter']
   );
 }
